de5ed6cf37d272142ce213e99e07e03246403c59
nati kamusher <natan.kamusher@gmail.com>
2022-07-16 10:42:21 +0300

    created container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 module.exports = {
 	module: {
@@ -31,6 +32,11 @@ module.exports = {
 	},
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, '../', 'dist'),
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+	]
 };
---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+const commonConfig = require('./webpack.common');
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, '../', 'dist'),
+    }, 
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
+            },
+            shared: packageJson.dependencies
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, prodConfig);
