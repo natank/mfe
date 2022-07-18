43d51658cdcf10b88eecd10f4bf46c8b8389f896
Nati Kamusher <natan.kamusher@gmail.com>
2022-07-18 19:47:33 +0300

    added header

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 export default () => {
-	return <div>
-		<h1>Hi therre!</h1>
-		<hr />
-		<MarketingApp />
-	</div>;
+	return (
+		<BrowserRouter>
+			<div>
+				<Header signedIn={9} onSignOut={() => { }} />
+				<MarketingApp />
+			</div>
+		</BrowserRouter>
+	);
 };
