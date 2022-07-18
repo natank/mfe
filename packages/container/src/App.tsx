674a19e82ccc2c662f5191ab211e8ea50d0f858c
Nati Kamusher <natan.kamusher@gmail.com>
2022-07-18 21:21:33 +0300

    randomized production class name

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+	productionPrefix: 'co',
+})
+
 export default () => {
 	return (
 		<BrowserRouter>
-			<div>
-				<Header signedIn={9} onSignOut={() => { }} />
-				<MarketingApp />
-			</div>
+			<StylesProvider generateClassName={generateClassName}>
+				<div>
+					<Header signedIn={9} onSignOut={() => { }} />
+					<MarketingApp />
+				</div>
+			</StylesProvider>
 		</BrowserRouter>
 	);
 };
